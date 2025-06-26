c66091282bf4b1a37bdc0cd9b1b32c3c4d95395e
juduque <judduque@bancolombia.com.co>
2025-06-26 13:54:59 -0500

    feat: response state of ms

--- ms_test_ia/domain/usecase/src/main/java/co/com/bancolombia/usecase/checkstatus/CheckStatusUseCase.java
+++ ms_test_ia/domain/usecase/src/main/java/co/com/bancolombia/usecase/checkstatus/CheckStatusUseCase.java
@@ -1,14 +1,18 @@
 package co.com.bancolombia.usecase.checkstatus;
 
-import co.com.bancolombia.model.systemstatus.gateways.SystemStatusRepository;
+import co.com.bancolombia.model.systemstatus.SystemStatus;
 import lombok.RequiredArgsConstructor;
-import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
 public class CheckStatusUseCase {
 
+    private static final String UP_STATUS = "UP";
+    private static final String UP_MESSAGE = "The system is running";
 
-    public String checkStatus() {
-        return "OK";
+    public SystemStatus checkStatus() {
+        return SystemStatus.builder()
+                .status(UP_STATUS)
+                .message(UP_MESSAGE)
+                .build();
     }
 }
--- ms_test_ia/infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/Handler.java
+++ ms_test_ia/infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/Handler.java
@@ -1,6 +1,5 @@
 package co.com.bancolombia.api;
 
-import co.com.bancolombia.api.dto.Response;
 import co.com.bancolombia.api.dto.mapper.StatusResponseMapper;
 import co.com.bancolombia.usecase.checkstatus.CheckStatusUseCase;
 import lombok.RequiredArgsConstructor;
@@ -12,26 +11,15 @@ import reactor.core.publisher.Mono;
 @Component
 @RequiredArgsConstructor
 public class Handler {
-//private  final UseCase useCase;
-//private  final UseCase2 useCase2;
 
     private final CheckStatusUseCase checkStatusUseCase;
 
-    public Mono<ServerResponse> listenGETUseCase(ServerRequest serverRequest) {
+    public Mono<ServerResponse> getHealthCheck(ServerRequest serverRequest) {
         return
                 Mono.fromCallable(checkStatusUseCase::checkStatus)
                         .flatMap(status -> ServerResponse.ok()
-                                .bodyValue(StatusResponseMapper.buildResponse(status)));
-
-    }
-
-    public Mono<ServerResponse> listenGETOtherUseCase(ServerRequest serverRequest) {
-        // useCase2.logic();
-        return ServerResponse.ok().bodyValue("");
-    }
-
-    public Mono<ServerResponse> listenPOSTUseCase(ServerRequest serverRequest) {
-        // useCase.logic();
-        return ServerResponse.ok().bodyValue("");
+                                .bodyValue(StatusResponseMapper.buildResponseOK(status)))
+                        .onErrorResume(error -> ServerResponse.status(500)
+                                .bodyValue(StatusResponseMapper.buildResponseError(error.getMessage())));
     }
 }
